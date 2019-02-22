67ec21e676b72c3c39a9aa3a7fc7492074ee49a6
Bryant <cabrera.bryant@gmail.com>
2019-02-21 17:41:33 -0800

    Adds solution to query7.sql.

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -68,6 +68,10 @@ SELECT name FROM city
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
  -- landing dock.
+--for the flights lab
+-- SELECT * FROM airports
+--   WHERE city = 'Brasilia';
+--Presidente Juscelino Kubitschek International Airport
 
 
 -- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
