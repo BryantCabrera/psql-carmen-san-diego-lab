9a391f5a87de34a7dd641b4cf09e873ddffbc94c
Bryant <cabrera.bryant@gmail.com>
2019-02-21 16:37:48 -0800

    Adds solution to query5.sql.

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -47,7 +47,9 @@ SELECT name from city
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
+SELECT * FROM city
+    WHERE name LIKE 'Serra%'
+    AND countrycode IN (SELECT code FROM country WHERE continent = 'South America');
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
