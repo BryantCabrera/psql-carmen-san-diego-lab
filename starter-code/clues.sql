0878ddd319b8a24a8879dfc7b0c93bdf09691f3e
Bryant <cabrera.bryant@gmail.com>
2019-02-21 16:48:30 -0800

    Adds solution to query6.sql.

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -55,7 +55,10 @@ SELECT * FROM city
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
-
+SELECT localname FROM country
+    WHERE code IN (SELECT countrycode FROM city
+    WHERE name LIKE 'Serra%'
+    AND countrycode IN (SELECT code FROM country WHERE continent = 'South America'));
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
